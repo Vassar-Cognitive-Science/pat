224e77cfc015239f93f413db477d3ddef2b952b0
Josh de Leeuw <josh.deleeuw@gmail.com>
2023-06-20 12:16:11 -0400

    dummy sleep function for testing loading

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,14 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+function sleep(time: number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, time)
+    })
+  }
+
 export async function POST(request: NextRequest): Promise<NextResponse> {
+    await sleep(1000);
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
     const json = await response.json();
     return NextResponse.json(json.title);
