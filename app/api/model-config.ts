658f90d14db0bf7b65edf9799f4868e1d3242f93
Josh de Leeuw <josh.deleeuw@gmail.com>
2023-07-17 15:43:06 -0400

    working openai call

--- app/api/model-config.ts
+++ app/api/model-config.ts
@@ -1,21 +1,20 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
 import { ConversationChain } from "langchain/chains";
-import { BufferMemory } from "langchain/memory"
-import { SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate } from "langchain/prompts";
+import { BufferMemory } from "langchain/memory";
+import {
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "langchain/prompts";
 
 const model = new ChatOpenAI({
   modelName: "gpt-3.5-turbo",
   temperature: 0.9,
 });
 
-const chat = new ConversationChain({
-  llm: model,
-  memory: new BufferMemory({ returnMessages: true})
-})
-
-const generateSystemMessage = (): SystemChatMessage => {
-  return new SystemChatMessage(`
+const systemMessage = `
     You are a philosopher having a dialog with one of your students.
     You should argue that the mind is best understood through neuroscience
     and a reductionist approach. You should push the student to argue
@@ -23,14 +22,20 @@ const generateSystemMessage = (): SystemChatMessage => {
     support for their position. Be friendly, but firm.
 
     These are some excerpts that may be related to the conversation.
-    You can optionally use this information in your response.`
-  );
-};
+    You can optionally use this information in your response.
+    
+    {excerpts}`;
 
-const generateChatPrompt = (): ChatPromptTemplate => {
-  return ChatPromptTemplate.fromPromptMessages([
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+  SystemMessagePromptTemplate.fromTemplate(systemMessage),
+  new MessagesPlaceholder("history"),
+  HumanMessagePromptTemplate.fromTemplate("{input}"),
+]);
 
-  ])
-}
+const chat = new ConversationChain({
+  llm: model,
+  memory: new BufferMemory({ returnMessages: true, memoryKey: "history", inputKey: "input" }),
+  prompt: chatPrompt,
+});
 
-export { model, generateSystemMessage, generateChatPrompt };
+export { chat };
--- app/api/route.ts
+++ app/api/route.ts
@@ -3,28 +3,31 @@ import { NextResponse } from "next/server";
 import { FaissStore } from "langchain/vectorstores/faiss";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { HumanChatMessage, AIChatMessage } from "langchain/schema";
-import { model, generateSystemMessage } from "./model-config";
+import { chat } from "./model-config";
 
 const directory = "faiss-index";
 
 //const db = await FaissStore.loadFromPython(directory, new OpenAIEmbeddings());
 
 async function sleep(time: number) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, time)
-    })
-  }
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
-
   const requestBody = await request.json();
-  const humanMessage = new HumanChatMessage(requestBody.message);
-    // await sleep(1000);
-    // const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-    // const json = await response.json();
-    const response = await model.call([
-      humanMessage
-    ]);
-    const text = (response as AIChatMessage).text;
-    return NextResponse.json(text);
+  const humanMessage = requestBody.message;
+  // await sleep(1000);
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  // const json = await response.json();
+  const response = await chat.call({
+    input: humanMessage,
+    excerpts: "",
+  });
+
+  console.log({response})
+
+  const text = response.response;
+  return NextResponse.json(text);
 }
