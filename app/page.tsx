b9c5bf695fd686ca7ab7572e9e1c412660dce295
Josh de Leeuw <josh.deleeuw@gmail.com>
2023-07-19 21:58:13 -0400

    add localstorage for conversation history

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
 import { useState, useRef, useEffect } from "react";
 import ChatMessage from "./components/chatmessage";
-import '@fortawesome/fontawesome-free/css/all.css';
+import "@fortawesome/fontawesome-free/css/all.css";
 
 interface Message {
   message: string;
@@ -32,14 +32,20 @@ function handlePrint() {
 }
 
 export default function Page() {
-
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
-  const [messages, setMessages] = useState<Message[]>([
-    { message: "Hi there!", sender: "Pat" }
-  ]);
+  const [messages, setMessages] = useState<Message[]>(()=>{
+    const storedMessages = localStorage.getItem("messages");
+    if (storedMessages) {
+      return JSON.parse(storedMessages);
+    }
+    return [{
+      message: startingMessage,
+      sender: "Pat",
+    }];
+  });
 
   const [newMessage, setNewMessage] = useState<Message>({
     message: "",
@@ -66,8 +72,12 @@ export default function Page() {
     setMessages([...messages, newMessage, response]);
     setIsSending(false);
     console.log(inputRef.current);
-    inputRef.current?.focus()
+    inputRef.current?.focus();
   };
+
+  useEffect(()=>{
+    localStorage.setItem("messages", JSON.stringify(messages));
+  }, [messages])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -85,49 +95,64 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen w-screen max-w-4xl bg-gray-100 p-4 mx-auto">
-      <div className="flex justify-between items-center mb-4">
-        <div></div>
-        <div className="text-center">
-          <h1 className="text-2xl font-bold">Pat</h1>
-          <h2 className="text-md font-light">Philosophical Artificial Thinker</h2>
+    <div className="flex flex-col h-screen w-screen mx-auto">
+      <div className="bg-gray-200 p-4">
+        <div className="flex justify-between items-center mb-4">
+          <div></div>
+          <div className="text-center">
+            <h1 className="text-2xl font-bold">Pat</h1>
+            <h2 className="text-md font-light">
+              Philosophical Artificial Thinker
+            </h2>
+          </div>
+          <button
+            id="print-button"
+            className="flex items-center justify-center w-10 h-10 bg-gray-500 text-white rounded-full"
+            onClick={handlePrint}
+          >
+            <i className="fas fa-print"></i>
+          </button>
         </div>
-        <button id="print-button" className="flex items-center justify-center w-10 h-10 bg-gray-500 text-white rounded-full" onClick={handlePrint}>
-          <i className="fas fa-print"></i>
-        </button>
       </div>
-      <div className="flex-1 overflow-y-scroll">
+      <div className="w-screen max-w-3xl mx-auto flex-1 flex flex-col justify-end py-4">
+      <div className="flex-1 overflow-y-auto">
         {!isLoaded && <p>Loading...</p>}
-        {isLoaded && messages.map((message, index) => (
-          <ChatMessage
-            key={index}
-            sender={message.sender}
-            message={message.message}
-          />
-        ))}
+        {isLoaded &&
+          messages.map((message, index) => (
+            <ChatMessage
+              key={index}
+              sender={message.sender}
+              message={message.message}
+            />
+          ))}
       </div>
       <div id="chat-input" className="flex">
-      <textarea
-        className="flex-1 mr-2 px-2 py-1 rounded-md border border-gray-300 resize-y overflow-y-auto"
-        placeholder="Type a message..."
-        value={newMessage.message}
-        onChange={handleInputChange}
-        disabled={isSending}
-        ref={inputRef}
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            handleSendClick();
-          }
-        }}
-      />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleSendClick}
-        disabled={isSending}
-      >
-        {isSending ? <i className="fas fa-spinner fa-spin"></i> : <i className="fas fa-paper-plane"></i>}
-      </button>
-</div>
+        <textarea
+          className="flex-1 mr-2 px-2 py-1 rounded-md border border-gray-300 resize-y overflow-y-auto"
+          placeholder="Type a message..."
+          value={newMessage.message}
+          onChange={handleInputChange}
+          disabled={isSending}
+          ref={inputRef}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleSendClick();
+            }
+          }}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleSendClick}
+          disabled={isSending}
+        >
+          {isSending ? (
+            <i className="fas fa-spinner fa-spin"></i>
+          ) : (
+            <i className="fas fa-paper-plane"></i>
+          )}
+        </button>
+      </div>
+      </div>
     </div>
   );
 }
